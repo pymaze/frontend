5a65093e70a11811e89de4a36ce3f43a57ef5a6c
Andrew Richardson <andrewlowellr@gmail.com>
2019-07-29 17:11:50 -0600

    Initial stab at visualization.

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,74 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class SecondPage extends Component {
+  
+  componentDidMount() {
+    let gridData = this.gridData();
+    var grid = d3.select("#grid")
+      .append("svg")
+      .attr("width","510px")
+      .attr("height","510px");
+    var row = grid.selectAll(".row")
+      .data(gridData)
+      .enter().append("g")
+      .attr("class", "row");
+    var column = row.selectAll(".square")
+      .data(function(d) { return d; })
+      .enter().append("rect")
+      .attr("class","square")
+      .attr("x", function(d) { return d.x; })
+      .attr("y", function(d) { return d.y; })
+      .attr("width", function(d) { return d.width; })
+      .attr("height", function(d) { return d.height; })
+      .style("fill", "#fff")
+      .style("stroke", "#222");
+  }
+
+  gridData() {
+    var data = new Array();
+    var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
+    var ypos = 1;
+    var width = 50;
+    var height = 50;
+
+    // iterate for rows 
+    for (var row = 0; row < 10; row++) {
+        data.push( new Array() );
+
+        // iterate for cells/columns inside rows
+        for (var column = 0; column < 10; column++) {
+            data[row].push({
+                x: xpos,
+                y: ypos,
+                width: width,
+                height: height
+            })
+            // increment the x position. I.e. move it over by 50 (width variable)
+            xpos += width;
+        }
+        // reset the x position after a row is complete
+        xpos = 1;
+        // increment the y position for the next row. Move it down 50 (height variable)
+        ypos += height; 
+    }
+    return data;
+  }
+
+  render() {
+    return (
+      <Layout>
+        <script src="https://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+        <SEO title="The Maze" />
+        <h1>Hi from the second page</h1>
+        <div id="grid"></div>
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
 
 export default SecondPage
