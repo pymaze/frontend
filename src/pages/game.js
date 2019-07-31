424fcdd5b22e50641462d7399e869c3a9753aea0
Garrett Weems <gwgraphicdesign@gmail.com>
2019-07-31 18:05:03 -0400

    💄 Dynamic game styling
    Game width / height calculated from maz data

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,47 +1,97 @@
 import React, { Component } from "react"
 import { Link } from "gatsby"
+import { GiSwordman } from "react-icons/gi"
 import axios from "axios"
 
-import { GiSwordman } from "react-icons/gi";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import { backendURL } from "../../gatsby-config"
 
-import "./game.css"
+import styled from "styled-components"
+
+const GameWrapper = styled.div`
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+const GameSquare = styled.div`
+  border-style: solid;
+  height: ${props => `calc(100% / ${props.rows}em)`};
+  border-color: black;
+  border-top-width: ${props =>
+    props.direction.includes("north") ? `1px` : "0px"};
+  border-right-width: ${props =>
+    props.direction.includes("east") ? `1px` : "0px"};
+  border-bottom-width: ${props =>
+    props.direction.includes("south") ? `1px` : "0px"};
+  border-left-width: ${props =>
+    props.direction.includes("west") ? `1px` : "0px"};
+`
+
+const GameBox = styled.div`
+  display: grid;
+  grid-template-columns: ${props => `repeat(${props.columns}, 1fr)`};
+  grid-auto-rows: 10em;
+  ${GameSquare} {
+    height: 100%;
+  }
+`
 
 class SecondPage extends Component {
   state = {
     playerX: 0,
     playerY: 0,
     error_msg: "",
-    name: "",           // *** Name of the logged-in player
-    title: "",          // *** Title of the current room
-    description: "",    // *** Description of the current room
-    players: [],        // *** Players in the current room
+    name: "", // *** Name of the logged-in player
+    title: "", // *** Title of the current room
+    description: "", // *** Description of the current room
+    players: [], // *** Players in the current room
     maze: [
-      [{ "n": false, "e": false, "s": true, "w": false }, { "n": false, "e": true, "s": false, "w": false }, { "n": false, "e": false, "s": true, "w": true }, { "n": false, "e": false, "s": false, "w": false }],
-      [{ "n": true, "e": true, "s": false, "w": false }, { "n": false, "e": false, "s": true, "w": true }, { "n": true, "e": true, "s": false, "w": false }, { "n": false, "e": false, "s": false, "w": true }],
-      [{ "n": false, "e": false, "s": true, "w": false }, { "n": true, "e": false, "s": true, "w": false }, { "n": false, "e": false, "s": false, "w": false }, { "n": false, "e": false, "s": false, "w": false }],
-      [{ "n": true, "e": true, "s": false, "w": false }, { "n": true, "e": false, "s": false, "w": true }, { "n": false, "e": true, "s": true, "w": false }, { "n": false, "e": false, "s": false, "w": true }]
+      [
+        { n: false, e: false, s: true, w: false },
+        { n: false, e: true, s: false, w: false },
+        { n: false, e: false, s: true, w: true },
+        { n: false, e: false, s: false, w: false },
+      ],
+      [
+        { n: true, e: true, s: false, w: false },
+        { n: false, e: false, s: true, w: true },
+        { n: true, e: true, s: false, w: false },
+        { n: false, e: false, s: false, w: true },
+      ],
+      [
+        { n: false, e: false, s: true, w: false },
+        { n: true, e: false, s: true, w: false },
+        { n: false, e: false, s: false, w: false },
+        { n: false, e: false, s: false, w: false },
+      ],
+      [
+        { n: true, e: true, s: false, w: false },
+        { n: true, e: false, s: false, w: true },
+        { n: false, e: true, s: true, w: false },
+        { n: false, e: false, s: false, w: true },
+      ],
     ],
-    backendURL: "https://build-week-civil-disobedients.herokuapp.com"
+    backendURL: "https://build-week-civil-disobedients.herokuapp.com",
   }
 
   componentDidMount() {
-    document.addEventListener("keydown", this.keyPressed, false);
-    this.getRooms();
+    document.addEventListener("keydown", this.keyPressed, false)
+    this.getRooms()
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keydown", this.keyPressed, false);
+    document.removeEventListener("keydown", this.keyPressed, false)
   }
 
   getRooms = () => {
-      axios.get(`${this.state.backendURL}/api/rooms`, { headers: { "Access-Control-Allow-Origin": "*" } })
-    // axios.get(`${this.state.backendURL}/api/rooms`, { headers: { "Authorization": "Bearer " + "6493c3550c33600a9445e035f5a06a5648bbc3ce"} })
+    axios
+      .get(`${this.state.backendURL}/api/rooms`, {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      })
       .then(res => {
         console.log(res.data)
-        // this.setState({ maze: res.data.rooms });
       })
       .catch(err => {
         console.log(err)
@@ -49,10 +99,15 @@ class SecondPage extends Component {
   }
 
   initialize = () => {
-    axios.get(`${this.state.backendURL}/api/adv/init`, { headers: { "Authorization": "Bearer 6493c3550c33600a9445e035f5a06a5648bbc3ce"} })
+    axios
+      .get(`${this.state.backendURL}/api/adv/init`, {
+        headers: {
+          Authorization: "Bearer 6493c3550c33600a9445e035f5a06a5648bbc3ce",
+        },
+      })
       .then(res => {
-        const { uuid, ...newState} = res.data;
-        this.setState(newState);
+        const { uuid, ...newState } = res.data
+        this.setState(newState)
       })
       .catch(err => {
         console.log(err)
@@ -60,88 +115,116 @@ class SecondPage extends Component {
   }
 
   move = direction => {
-    // axios.post("https://lambda-mud-test.herokuapp.com/api/adv/move", { direction: direction}, { headers: { "Authorization": "Bearer 6493c3550c33600a9445e035f5a06a5648bbc3ce"} })
-    //   .then(res => {
-    //     if (res.data.error_msg.length > 0) {
-    //       console.log(res.data.error_msg)
-    //     } else {
-    //       this.setState(res.data);
-    //     }
-    //   })
-    //   .catch(err => {
-    //     console.log(err)
-    //   })
-    switch(direction) {
+    switch (direction) {
       case "n":
-        if (this.state.playerY > 0 && !this.state.maze[this.state.playerY][this.state.playerX].n) {
+        if (
+          this.state.playerY > 0 &&
+          !this.state.maze[this.state.playerY][this.state.playerX].n
+        ) {
           this.setState({ playerY: this.state.playerY - 1 })
         }
-        break;
+        break
       case "e":
-        if (this.state.playerX < this.state.maze[0].length - 1 && !this.state.maze[this.state.playerY][this.state.playerX].e) {
+        if (
+          this.state.playerX < this.state.maze[0].length - 1 &&
+          !this.state.maze[this.state.playerY][this.state.playerX].e
+        ) {
           this.setState({ playerX: this.state.playerX + 1 })
         }
-        break;
+        break
       case "s":
-        if (this.state.playerY < this.state.maze.length - 1 && !this.state.maze[this.state.playerY][this.state.playerX].s) {
+        if (
+          this.state.playerY < this.state.maze.length - 1 &&
+          !this.state.maze[this.state.playerY][this.state.playerX].s
+        ) {
           this.setState({ playerY: this.state.playerY + 1 })
         }
-        break;
+        break
       case "w":
-        if (this.state.playerX > 0 && !this.state.maze[this.state.playerY][this.state.playerX].w) {
+        if (
+          this.state.playerX > 0 &&
+          !this.state.maze[this.state.playerY][this.state.playerX].w
+        ) {
           this.setState({ playerX: this.state.playerX - 1 })
         }
-        break;
+        break
       default:
-        break;
+        break
     }
     console.log(this.state.playerX, this.state.playerY)
   }
 
   keyPressed = e => {
     if (e.key === "ArrowUp" || e.key === "w") {
-      e.preventDefault();
-      this.move("n");
+      e.preventDefault()
+      this.move("n")
     }
     if (e.key === "ArrowDown" || e.key === "s") {
-      e.preventDefault();
-      this.move("s");
+      e.preventDefault()
+      this.move("s")
     }
     if (e.key === "ArrowLeft" || e.key === "a") {
-      e.preventDefault();
-      this.move("w");
+      e.preventDefault()
+      this.move("w")
     }
     if (e.key === "ArrowRight" || e.key === "d") {
-      e.preventDefault();
-      this.move("e");
+      e.preventDefault()
+      this.move("e")
     }
   }
 
   render() {
-    const { playerX, playerY, name, title, description, players } = this.state;
+    const { playerX, playerY, name, title, description, players } = this.state
+
+    const GameRows = () =>
+      this.state.maze.map((row, row_i) =>
+        row.map((cell, col_i) => {
+          const processedClass = `${cell.n || row_i === 0 ? "north" : ""}${
+            cell.e || col_i === row.length - 1 ? " east" : ""
+          }${cell.s || row_i === this.state.maze.length - 1 ? " south" : ""}${
+            cell.w || col_i === 0 ? " west" : ""
+          }`
+
+          return row_i === playerY && col_i === playerX ? (
+            <GameSquare
+              key={row_i + col_i}
+              direction={processedClass}
+              data-x={row_i}
+              data-y={col_i}
+            >
+              <GiSwordman />
+            </GameSquare>
+          ) : (
+            <GameSquare key={row_i + col_i} direction={processedClass} />
+          )
+        })
+      )
+
+    const getColumns = maze => maze[0].length
+    const getRows = maze => maze.length
+
     return (
       <Layout>
         <script src="https://d3js.org/d3.v3.min.js" charSet="utf-8"></script>
         <SEO title="The Maze" />
         <h1>Hi from the second page</h1>
-        <div id="mapBox">
-          {this.state.maze.map((row, row_index) => {
-            return row.map((cell, column_index) => {
-              const processedClass = `mapCell${cell.n || row_index === 0 ? " north" : ""}${cell.e || column_index === row.length-1 ? " east" : ""}${cell.s || row_index === this.state.maze.length-1 ? " south" : ""}${cell.w || column_index === 0 ? " west" : ""}`
-              if (row_index === playerY && column_index === playerX) {
-                return <span key={row_index + column_index} className={processedClass} data-x={row_index} data-y={column_index}><GiSwordman /></span>
-              }
-              return <span key={row_index + column_index} className={processedClass} data-x={row_index} data-y={column_index} />
-            })
-          })}
-        </div>
+        <GameWrapper>
+          <GameBox
+            columns={getColumns(this.state.maze)}
+            rows={getRows(this.state.maze)}
+          >
+            <GameRows />
+          </GameBox>
+        </GameWrapper>
         <div>
           <h2>{name}</h2>
           <p>{title}</p>
           <p>{description}</p>
           <p>
             Players:
-            {players.map(p => <span key={p}>{" " + p}</span>)}
+            {players.map(p => (
+              <span key={p}>{" " + p}</span>
+            ))}
           </p>
         </div>
         <Link to="/">Go back to the homepage</Link>
